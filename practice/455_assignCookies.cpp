ab76d3875c86ef4b0d7312a1a2e2529de228336a
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-07-15 11:17:50 -0400

    455 completed

---
+++ practice/455_assignCookies.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int i = 0, j = 0, counter = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        while(i < g.size() && j < s.size()){
+            if(g[i]<=s[j]){
+                i++;
+                counter++;
+            }
+            j++;
+        }
+        return counter;
+    }
+};
