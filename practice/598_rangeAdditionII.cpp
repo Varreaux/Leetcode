279807cda5594217a188e132e054bfc9715cad3d
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-07-26 09:40:17 -0400

    598 version 2 completed

--- practice/598_rangeAdditionII.cpp
+++ practice/598_rangeAdditionII.cpp
@@ -15,4 +15,14 @@ public:
         auto it = mm.begin(), itt = mmm.begin();
         return it->first*itt->first;
     }
+
+	int maxCount_v2(int m, int n, vector<vector<int>>& ops) {
+        if(ops.size()==0){return m*n;}
+        int min = ops[0][0], min2 = ops[0][1];
+        for(auto op:ops){
+            if(op[0]<min){min=op[0];}
+            if(op[1]<min2){min2=op[1];}
+        }
+        return min*min2;
+    }
 };
