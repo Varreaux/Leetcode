4477454d630193a18d4859fa4a324105a73057e8
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-07-29 19:45:40 -0400

    605 completed

---
+++ practice/605_canPlaceFlowers.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if(flowerbed.size()==1&&flowerbed[0]==0)
+            return true;
+        for(int i = 0; i < flowerbed.size(); i++){
+            if(i==0){
+                if(flowerbed[i]==0&&flowerbed[i+1]==0){
+                    flowerbed[i]=1;
+                    n--;
+                }
+            }
+            else if(i==flowerbed.size()-1){
+                if(flowerbed[i]==0&&flowerbed[i-1]==0)
+                    n--;
+            }
+            else if(flowerbed[i-1]==0&&flowerbed[i]==0&&flowerbed[i+1]==0){
+                    flowerbed[i]=1;
+                    n--;
+            }
+        } 
+        if(n<=0)
+            return true;
+        return false;
+    }
+};
