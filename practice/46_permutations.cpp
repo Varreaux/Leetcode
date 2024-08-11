e7d36eff9ffc99f8e03c6d3206c9305809f144f6
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-08-11 08:17:14 -0400

    46 completed

---
+++ practice/46_permutations.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> answer;
+        sort(nums.begin(),nums.end());
+        do
+            answer.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end()));
+        return answer;
+    }
+};
