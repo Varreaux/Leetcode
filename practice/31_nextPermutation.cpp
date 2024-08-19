f5cf4fc4818a6715e4ae971c7820ad0f2a799b8f
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-08-19 15:29:43 -0400

    31 organized the code a bit  more

--- practice/31_nextPermutation.cpp
+++ practice/31_nextPermutation.cpp
@@ -6,40 +6,30 @@ using namespace std;
 class Solution {
 public:
     void nextP(vector<int> &nums, int &j, int &i){
-        bool changed = false;
         for(int l = i-1; l > j+1; l--){
             for(int k = l-1; k > j; k--){
                 if(nums[k]<nums[l]){
                     j = k;
                     i = l;
                     nextP(nums, j, i);
-                    changed = true;
-                    break;
+                    return;
                 }
             }
-            if(changed)
-                break;
         }
-        return;
     }
 
     void nextPermutation(vector<int>& nums) {
-        bool changed = false;
         for(int i = nums.size()-1; i >= 1; i--){
             for(int j = i - 1; j >= 0; j--){
                 if(nums[j]<nums[i]){
                     nextP(nums, j, i);
-                    changed = true;
                     swap(nums[j], nums[i]);
                     sort(nums.begin()+j+1, nums.end());
-                    break;
+                    return;
                 }
             }
-            if(changed)
-                break;
         }
-        if(!changed)
-            sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
     }
 };
 
