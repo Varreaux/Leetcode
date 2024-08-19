e582b337514cb1213db6ea3ea02c4dafe1951a0a
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-08-19 14:54:11 -0400

    31 alternative answer

--- practice/31_nextPermutation.cpp
+++ practice/31_nextPermutation.cpp
@@ -1,41 +1,51 @@
 #include<vector>
-#include<algorithm>
+// #include<algorithm>
 
 using namespace std;
 
 class Solution {
 public:
+    void nextP(vector<int> &nums, int &j, int &i){
+        bool changed = false;
+        for(int l = i-1; l > j+1; l--){
+            for(int k = l-1; k > j; k--){
+                if(nums[k]<nums[l]){
+                    j = k;
+                    i = l;
+                    nextP(nums, j, i);
+                    changed = true;
+                    break;
+                }
+            }
+            if(changed)
+                break;
+        }
+        return;
+    }
+
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        bool changed = false;
+        for(int i = nums.size()-1; i >= 1; i--){
+            for(int j = i - 1; j >= 0; j--){
+                if(nums[j]<nums[i]){
+                    nextP(nums, j, i);
+                    changed = true;
+                    swap(nums[j], nums[i]);
+                    sort(nums.begin()+j+1, nums.end());
+                    break;
+                }
+            }
+            if(changed)
+                break;
+        }
+        if(!changed)
+            sort(nums.begin(), nums.end());
     }
 };
 
 // class Solution {
 // public:
 //     void nextPermutation(vector<int>& nums) {
-//         bool changed = false;
-//         for(int i = nums.size()-1; i >= 1; i--){
-//             for(int j = i - 1; j >= 0; j--){
-//                 if(nums[j]<nums[i]){
-//                     int temp1 = j+1;
-//                     int temp2 = i-1;
-//                     while(temp1 < temp2){
-//                         if(nums[temp1]<nums[temp2]){
-//                             j = temp1;
-//                             i = temp2;
-//                         }
-//                         temp1++;
-//                     }
-//                     changed = true;
-//                     swap(nums[j], nums[i]);
-//                     sort(nums.begin()+j+1, nums.end());
-//                     break;
-//                 }
-//             }
-//             if(changed)
-//                 break;
-//         }
-//         if(!changed)
-//             sort(nums.begin(), nums.end());
+//         next_permutation(nums.begin(),nums.end());
 //     }
 // };
