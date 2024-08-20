fedcf704be4b17731feb0304a623e937e333de1e
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-08-20 11:33:16 -0400

    53 complete using kadane

--- practice/53_maximumSubarray.cpp
+++ practice/53_maximumSubarray.cpp
@@ -3,24 +3,34 @@ using namespace std;
 
 class Solution {
 public:
+
     int maxSubArray(vector<int>& nums) {
-        int b=0, maxSum=0, a, sum;
-        for(int num:nums)
-            maxSum+=num;
-        while (b<nums.size()){
-            a = 0, sum = 0;
-            for(int i = 0; i <=b; i++)
-                sum+=nums[i];
-            if(maxSum<sum)
-                maxSum=sum;
-            int bb = b++;
-            while(bb<nums.size()-1){
-                sum-=nums[a++];
-                sum+=nums[++bb];
-                if(maxSum<sum)
-                    maxSum=sum;
-            }
+        int maxSum=nums[0], sum=0;
+        for(int num:nums){
+            sum = max(num, (sum+num));
+            maxSum = max(maxSum, sum);
         }
         return maxSum;
     }
+
+    // int maxSubArray(vector<int>& nums) {
+    //     int b=0, maxSum=0, a, sum;
+    //     for(int num:nums)
+    //         maxSum+=num;
+    //     while (b<nums.size()){
+    //         a = 0, sum = 0;
+    //         for(int i = 0; i <=b; i++)
+    //             sum+=nums[i];
+    //         if(maxSum<sum)
+    //             maxSum=sum;
+    //         int bb = b++;
+    //         while(bb<nums.size()-1){
+    //             sum-=nums[a++];
+    //             sum+=nums[++bb];
+    //             if(maxSum<sum)
+    //                 maxSum=sum;
+    //         }
+    //     }
+    //     return maxSum;
+    // }
 };
