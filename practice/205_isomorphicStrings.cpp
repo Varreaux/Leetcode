4d627dae43c03e70c2bdf8e0ca7cd0ff54b83540
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-10-16 09:17:50 -0400

    205 version 2 -- too slow

--- practice/205_isomorphicStrings.cpp
+++ practice/205_isomorphicStrings.cpp
@@ -3,22 +3,37 @@
 
 using namespace std;
 
-
-//The issue is that this only works if the strings have alpha chars in them....need to rework this.
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
+    
+	//This works for alphanumerical but fails on the last test case cause it's too slow!
+	bool isIsomorphic(string s, string t) {
         if(s.length()!=t.length()){return false;}
-        vector<char>mapping(26,'-');
-        vector<char>track(26,'-');
+        vector<pair<char, char>> vec;
         for(int i = 0; i < s.length(); i++){
-            if((mapping[s[i]-'a']=='-' || mapping[s[i]-'a']== t[i]) && (track[t[i]-'a'] == '-' || track[t[i]-'a'] == s[i])){
-                track[t[i]-'a']= s[i];
-                mapping[s[i]-'a']= t[i];
+            for(auto p : vec){
+                if(p.first == s[i] && p.second != t[i]){return false;}
+                if(p.second == t[i] && p.first != s[i]){return false;}
             }
-            else
-                return false;
+            vec.push_back(make_pair(s[i], t[i]));
         }
         return true;
     }
+	
+	////The issue is that this only works if the strings have alpha chars in them....need to rework this.
+	
+	// bool isIsomorphic(string s, string t) {
+    //     if(s.length()!=t.length()){return false;}
+    //     vector<char>mapping(26,'-');
+    //     vector<char>track(26,'-');
+    //     for(int i = 0; i < s.length(); i++){
+    //         if((mapping[s[i]-'a']=='-' || mapping[s[i]-'a']== t[i]) && (track[t[i]-'a'] == '-' || track[t[i]-'a'] == s[i])){
+    //             track[t[i]-'a']= s[i];
+    //             mapping[s[i]-'a']= t[i];
+    //         }
+    //         else
+    //             return false;
+    //     }
+    //     return true;
+    // }
 };
