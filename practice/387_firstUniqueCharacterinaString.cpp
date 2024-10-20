7ddce18356af259d7e851f66cd4dbe1bf16023cd
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-10-20 12:37:19 -0400

    387 optimized

--- practice/387_firstUniqueCharacterinaString.cpp
+++ practice/387_firstUniqueCharacterinaString.cpp
@@ -1,11 +1,22 @@
 #include<string>
+#include<vector>
 #include<unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
+
     int firstUniqChar(string s) {
+        vector<int> vec(26, 0);
+        for(char c: s)
+            vec[c-'a']++;
+        for(int i = 0; i < s.length(); i++)
+            if(vec[s[i]-'a']==1){return i;}
+        return -1;
+    }
+    //slower:
+	int firstUniqChar_v2(string s) {
         unordered_map<char, int> m;
         for(char c: s)
             m[c]++;
