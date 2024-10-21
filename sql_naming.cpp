0c2f57a6ed7021253c11ec033d003026a33b2122
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-10-21 14:21:13 -0400

    adding the file and the naming code

---
+++ sql_naming.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+string change (string title);
+
+int main (){
+
+    string str = "1757. Recyclable and Low Fat Products";
+    string answer = change(str);
+    string realAnswer = "SQL/" + answer; 
+    
+
+    ofstream outFile;
+    outFile.open(realAnswer);
+    cout<<answer<<endl<<endl;
+
+}
+
+string change (string title){
+    bool done = false;
+    for(int i = 0; i < title.length()-1; i++){
+        if(title[i] == '.')
+            title[i]='_';
+        if(title[i]== ' ')
+            title.erase(i, 1);
+        if(title[i]<91 && title[i]>64 && !done){
+            title[i] = title[i] + 32;
+            done = true;
+        }
+    }
+    return title+".sql";
+}
