8d95a51fc14562c001fb819f8cc979118ae45b7a
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-11-08 10:20:09 -0500

    197 alternative version

--- SQL/197_risingTemperature.sql
+++ SQL/197_risingTemperature.sql
@@ -1,3 +1,9 @@
 Select w1.id from weather w1, weather w2
 where datediff(w1.recorddate, w2.recorddate) = 1 and w1.temperature>w2.temperature
 
+
+-- Same thing
+SELECT w1.id
+FROM weather w1
+JOIN weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
