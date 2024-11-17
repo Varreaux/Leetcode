2dad01670d5475286c00e3de203730bb96b85cb3
Varreaux <116587683+Varreaux@users.noreply.github.com>
2024-11-17 08:07:41 -0500

    771 completed

---
+++ practice/771_jewelsandStones.cpp
@@ -0,0 +1,13 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count = 0;
+        for( char c : stones){
+            if(jewels.find(c)!=string::npos) count++;
+        }
+        return count;
+    }
+};
