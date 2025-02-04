a9389a8adec27c144feca3cb464fb3a07df73e2f
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-02-04 08:24:25 -0500

    1217 completed

---
+++ practice/1217_minimumCosttoMoveChipstoTheSamePosition.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even = 0, odd = 0;
+        for(int num : position){
+            if(num%2==0) even++;
+            else odd++;
+        }
+        return min(even, odd);
+    }
+};
