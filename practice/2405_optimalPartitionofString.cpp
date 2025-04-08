c3b5aef1ea49dc519bdd71cea386bf47f682f2ec
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-04-08 14:57:04 -0400

    2405 completes

---
+++ practice/2405_optimalPartitionofString.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+class Solution {
+	public:
+		int partitionString(string s) {
+			bool seen[26] = {}; int c = 0;
+			for(int i = 0; i < s.length(); i++){
+				if(seen[s[i]-'a']){
+					fill(begin(seen),end(seen), false);
+					c++;
+				}
+				seen[s[i]-'a'] = true;
+			}
+			return ++c;
+		}
+	};
