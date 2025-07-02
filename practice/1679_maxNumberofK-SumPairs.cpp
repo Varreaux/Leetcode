2f1eb4394550966f5248a7fe142b84554b3bd78d
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-07-02 14:45:13 -0400

    1679 completed

---
+++ practice/1679_maxNumberofK-SumPairs.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	//nlogn
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i = 0, j = nums.size()-1, count=0;
+        while(i<j){
+            if(nums[i]+nums[j]>k) j--;
+            else if(nums[i]+nums[j]<k) i++;
+            else{
+                i++;j--;count++;
+            }
+        }
+        return count;
+    }
+};
