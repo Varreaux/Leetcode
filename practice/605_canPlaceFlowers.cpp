1f289f922e8a59fafe7b69bc83750abc50dca082
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-07-02 15:08:55 -0400

    605 rewritten

--- practice/605_canPlaceFlowers.cpp
+++ practice/605_canPlaceFlowers.cpp
@@ -26,4 +26,15 @@ public:
             return true;
         return false;
     }
+
+    bool canPlaceFlowersV2(vector<int>& flowerbed, int n) {
+        if(n==0)return true;
+        for(int i = 0; i<flowerbed.size(); i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.size()-1 || flowerbed[i+1]==0)){
+                flowerbed[i]=1;
+                if(--n==0)return true;
+            }
+        }
+        return false;
+    }
 };
