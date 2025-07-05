83c4ddfbedb2ca8b23c94dee52b41a5a2b720c84
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-07-05 08:17:51 -0400

    643 little cleaner solution

--- practice/643_maximumAverageSubarrayI.cpp
+++ practice/643_maximumAverageSubarrayI.cpp
@@ -18,4 +18,17 @@ public:
         }
         return max/k;
     }
+
+    //same thing, just a little cleaner
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum = 0.0;
+        for(int i = 0; i<k; i++){sum+=nums[i];}
+        double max = sum;
+        for (int i = k; i < nums.size(); i++){
+            sum+=nums[i];
+            sum-=nums[i-k];
+            if(sum>max){max=sum;}
+        }
+        return max/k;
+    }
 };
