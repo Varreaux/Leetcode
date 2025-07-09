47bc6d1389636d2300f4db81bb57604df63662a1
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-07-09 10:06:57 -0400

    724 completed

--- practice/724_findPivotIndex.cpp
+++ practice/724_findPivotIndex.cpp
@@ -1,34 +1,17 @@
 #include<vector>
+#include <numeric>
+
 using namespace std;
 
 class Solution {
 public:
-
     int pivotIndex(vector<int>& nums) {
-        int sum = 0, sum1 = 0;
-        for (int i = 1; i < nums.size(); i++)
-            sum+=nums[i];
-        if(sum==sum1)
-            return 0;
-        for (int i = 1; i < nums.size(); i++){
-            sum1+=nums[i-1];
-            sum-=nums[i];
-            if(sum==sum1)
-                return i;
+        int left = 0, right = accumulate(nums.begin(), nums.end(), 0);
+        for(int i = 0; i < nums.size(); i++){
+            right -= nums[i];
+            if(i!=0)left+= nums[i-1];
+            if(right == left) return i;
         }
         return -1;
     }
-
-    // int pivotIndex(vector<int>& nums) {
-    //     for (int i = 0; i < nums.size(); i++){
-    //         int sum1 = 0, sum2 = 0;
-    //         for(int j = 0; j<i; j++)
-    //             sum1+=nums[j];
-    //         for(int k = i+1; k<nums.size(); k++)
-    //             sum2+=nums[k];
-    //         if(sum1==sum2)
-    //             return i;
-    //     }
-    //     return -1;
-    // }
 };
